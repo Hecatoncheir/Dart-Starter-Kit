2cd0e2349c8b02507661f4cdeee43c246c8b5e24
Rasarts <rasart.pro@gmail.com>
2015-03-07 20:40:43 +0300

    update for livereload and compass

---
+++ config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'build/web/assets/styles'
+sass_dir = 'web/assets/styles'
+images_dir = 'web/assets/images'
+javascripts_dir = 'web/assets/scripts'
+relative_assets = true
+line_comments = true
+output_style = :compressed
--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
 # Dart Starter Kit
 
 Designed to create the application structure
+
+
+Need Compass, CssComb, Sass, LiveReload, Dart
+
+install 
+gem guard-livereload
+gem guard-compass
+pip install livereload
+
+do not forgete: compass init
+start whith: guard -i
